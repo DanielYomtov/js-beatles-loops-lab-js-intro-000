d67e6681ed3d0c6bd6420c20f588efdaffc322bb
D Y <yomtovdaniel1@gmail.com>
2019-09-05 07:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicianArr, instrumentArr) {
+  let emptyArr = [];
+  for(let i = 0; i < musicianArr.length; i++){
+    let musicianName = musicianArr[i];
+    let instrumentName = instrumentArr[i];
+    emptyArr.push(musicianName + " plays " + instrumentName);
+  }
+  return emptyArr;
+}
+
+
+function johnLennonFacts(facts) {
+  let newArr = [];
+  let i = 0;
+  while(i < facts.length){
+    newArr.push(facts[i] + "!!!");
+    i++;
+  }
+  return newArr;
+}
+
+
+iLoveTheBeatles = (num) => {
+  let emptyArr = [];
+  do{
+    emptyArr.push("I love the beatles!");
+  } while(i < num);
+  let i = i + 1;
+}
